263f8d36c581ff00c3aa88ba302da5380ec2801c
Robin de Rooij <rderooij685@gmail.com>
2020-05-07 15:47:44 +0200

    Rename dirs again

--- build_script.sh
+++ build_script.sh
@@ -12,7 +12,7 @@ cp -r /usr/lib/swi-prolog app_dir/usr/lib/
 cp -L /usr/lib/x86_64-linux-gnu/libedit.so.2 app_dir/usr/lib/swi-prolog/lib/x86_64-linux/
 cp -L /lib/x86_64-linux-gnu/libpcre.so.3 app_dir/usr/lib/swi-prolog/lib/x86_64-linux/
 cp -L /usr/lib/x86_64-linux-gnu/libbsd.so.0 app_dir/usr/lib/swi-prolog/lib/x86_64-linux/
-rm -rf app_dir/usr/lib/swi-prolog/bin/amd64/swipl-ld
+rm -rf app_dir/usr/lib/swi-prolog/bin/x86_64-linux/swipl-ld
 git clone https://github.com/terminusdb/terminus_store_prolog.git "$TERMINUS_STORE_PROLOG_DIR"
 cd "$TERMINUS_STORE_PROLOG_DIR"
 ./make
@@ -20,4 +20,4 @@ rm -rf rust/target/release/build
 rm -rf rust/target/release/deps
 cd $CURRENT_DIR
 #linuxdeploy-x86_64.AppImage --appdir ./app_dir --executable /lib/swi-prolog/bin/x86_64-linux/swipl --library /lib/swi-prolog --library ~/.local/share/swi-prolog/pack/terminus_store_prolog/rust/target/release/libterminus_store_prolog.so -d terminusdb.desktop -i swipl.png --custom-apprun AppRun --output appimage --verbosity=0
-./linuxdeploy-x86_64.AppImage --appdir ./app_dir --executable /usr/lib/swi-prolog/bin/amd64/swipl --library "$TERMINUS_STORE_PROLOG_DIR/rust/target/release/libterminus_store_prolog.so" --library /lib/x86_64-linux-gnu/libpcre.so.3 -d terminusdb.desktop -i terminusdb.svg --custom-apprun AppRun --output appimage --verbosity=0
+./linuxdeploy-x86_64.AppImage --appdir ./app_dir --executable /usr/lib/swi-prolog/bin/x86_64-linux/swipl --library "$TERMINUS_STORE_PROLOG_DIR/rust/target/release/libterminus_store_prolog.so" --library /lib/x86_64-linux-gnu/libpcre.so.3 -d terminusdb.desktop -i terminusdb.svg --custom-apprun AppRun --output appimage --verbosity=0
